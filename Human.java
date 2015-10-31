08fed5677b5834b25aa47759c69f795276edbada
Riley Shenk <rjshenk@gmail.com>
2015-10-31 17:28:34 -0400

    seperated simulator and human class

--- Human.java
+++ Human.java
@@ -1,160 +1,150 @@
-package life;
-
-import java.util.LinkedList;
-import java.util.Random;
-
 public class Human {
-  public String name;
-  private int age;
-  private static int startYear;
-  private static int currentYear;
-  public int health; 
-  public String ethnicity;
-  public String gender;
-  public Human spouse;
-  public LinkedList<Human> children;
-  public LinkedList<Human> friends;
-  private Random rand;
-  public boolean employed;
-  public int salary;
-  
+    private final double AGE_PROBABILTY = 0.5;
+    private final double ETHNICITY_PROBABILITY = 0.20;
+    public String name;
+    private int age;
+    public int health;
+    public String ethnicity;
+    public String gender;
+    public Human spouse;
+    public LinkedList<Human> children;
+    public LinkedList<Human> friends;
+    private Random rand;
+    public boolean employed;
+    public int salary;
 
-
-
-  /**
-  * Initialize the instance variables for a human
-  */
-  public Human (String name, int age, String ethnicity, String gender, boolean employed, int salary) {
-      this.name = name;
-      startYear = currentYear = 2015;
-      this.age = age;
-      this.ethnicity = ethnicity;
-      this.gender = gender;
-      spouse = null;
-      children = new LinkedList<Human>();
-      friends = new LinkedList<Human>();
-      Random rnd = new Random();
-      rnd.setSeed(System.currentTimeMillis());
-  }
-  
-  public Human (String name, int age, String ethnicity, String gender, boolean employed, int salary, long seed) {
-      this(name, age, ethnicity, gender, employed, salary);
-      rand.setSeed(seed);
-  }
-  
-  /**
-   * increase the year by 1
-   */
-  public static void newYear() {
-      currentYear++;
-  }
-  
-  public static int getYear() {
-      return currentYear;
-  }
-  
-  public int getAge() {
-      return currentYear - startYear + age;
-  }
-  public void introduce() {
-      System.out.println("Hi, my name is " + name + " and I'm " + age + " years old");
-  }
-
-  public void marry (Human toWed) {
-      System.out.print(name + " asked " + toWed.name + " to marry ");
-      System.out.println(gender.equals("male") ? "him" : "her");
-      if (spouse == null && toWed.spouse == null) {
-          spouse = toWed;
-          toWed.spouse = this;
-          System.out.println("Marriage accepted");
-      } else {
-          System.out.println("Someone already has a spouse");
-      }
-  }
-  
-  public void divorce () {
-      if (spouse != null) {
-          System.out.println(name + " and " + spouse.name + " divorced");
-          spouse.spouse = null;
-          spouse = null;
-      } else {
-          System.out.println(name + " isn't even married");
-      }
-  }
-
-  public void giveBirth (Human child) {
-      if (!gender.equals("female")) {
-          System.out.println(name + " has to be a female");
-          return;
-      }
-      if (spouse == null) {
-          System.out.println(name + " needs to be married to give birth");
-          return;
-      }
-      if (!spouse.gender.equals("male")) {
-          System.out.println(name + " can only give birth if her spouse is a male");
-          return;
-      }
-      
-      System.out.println(name + " and " + spouse.name + " gave birth to a baby named " + child.name);
-      children.add(child);
-  }
-
-  public void getJob (int money) {
-      if (employed != true) {
-          employed = true;
-          salary = money;
-          System.out.println(name + " found a job that is paying " + (gender.equals("male") ? "him" : "her") + " " + salary + " per year");
-      } else {
-          System.out.println(name + " already has a job");
-      }
-  }
-
-  public void leaveJob () {
-      if (employed == true) {
-          employed = false;
-          salary = 0;
-          System.out.println(name + " has left " + (gender.equals("male") ? "his" : "her") + " job");
-      } else {
-          System.out.println(name + " does not have a job " + (gender.equals("male") ? "he" : "she") + " can leave");
-      }
-  }
-
-  public static void main(String[] args) {
-        public static void main(String[] args) {
-        ArrayList<Human> comunity = new ArrayList<Human>();
-        Human andrew, bethany, riley, abhijeet, ashwin, luc, kelsey, baby;
-
-        // Initialize the humans
-        comunity.add(new Human("Andrew", 23, "Vietnamese", "male", false, 0));
-        comunity.add(new Human("Bethany", 19, "Chinese", "female", true, 5000));
-        comunity.add(new Human("Riley", 23, "German", "male", false, 0));
-        comunity.add(new Human("Abhijeet", 23, "Indian", "male", false, 0));
-        comunity.add(new Human("Ashwin", 23, "Nepali", "male", true, 120000));
-        comunity.add(new Human("Luc", 27, "Vietnamese", "male", true, 70000));
-        comunity.add(new Human("Kelsey", 24, "Native American", "female", true, 50000));
-        comunity.add(new Human("Chuwali", 0, "Nepali", "female", false, 0));
-        Random rand = new Random();
+    /**
+     * Initialize the instance variables for a human
+     */
+    public Human(String name, int age, String ethnicity, String gender, boolean employed, int salary) {
+        this.name = name;
+        startYear = currentYear = 2015;
+        this.age = age;
+        this.ethnicity = ethnicity;
+        this.gender = gender;
+        spouse = null;
+        children = new LinkedList<Human>();
+        friends = new LinkedList<Human>();
+        rand = new Random();
         rand.setSeed(System.currentTimeMillis());
-        int year = 2015;
-        while (Human.getYear() < 2115) {
-            // each year you meet a friend
-            Human newFriend;
-            for (Human person : comunity) {
-                // each year you spend time with someone in your community.
-                // if you aren't already friends you may become friends
-                newFriend = comunity.get(rand.nextInt(comunity.size()));
-                person.makeNewFriend(newFriend);
-                
-                // if the person is married they may try to have a kid
-                person.
-                if (person.isMarried()) {
-                    person.hasChild();
-                }
-            }
-            Human.newYear();
+    }
+
+    public Human(String name, int age, String ethnicity, String gender, boolean employed, int salary, long seed) {
+        this(name, age, ethnicity, gender, employed, salary);
+        rand.setSeed(seed);
+    }
+
+    /**
+     * increase the year by 1
+     */
+    public void newYear() {
+        currentYear++;
+        if (getAge() < 40) {
+            health += rand.nextInt();
+        }
+
+    }
+
+    public int getAge() {
+        return currentYear - startYear + age;
+    }
+
+    public String introduce() {
+        return "Hi, my name is " + name + " and I'm " + age + " years old";
+    }
+
+    public String marry(Human toWed) {
+        String ret = new String();
+        ret += name + " asked " + toWed.name + " to marry " + (gender.equals("male") ? "him\n" : "her\n");
+        if (spouse == null && toWed.spouse == null) {
+            spouse = toWed;
+            toWed.spouse = this;
+            ret += "Marriage accepted";
+        } else {
+            ret += "Someone already has a spouse";
+        }
+        
+        return ret;
+    }
+
+    public String divorce() {
+        if (spouse != null) {
+            return name + " and " + spouse.name + " divorced";
+            spouse.spouse = null;
+            spouse = null;
+        } else {
+            return name + " isn't even married";
         }
     }
-  }
 
+    public String giveBirth(Human child) {
+        if (!gender.equals("female")) {
+            return name + " has to be a female";
+        }
+        if (spouse == null) {
+            return name + " needs to be married to give birth";
+        }
+        if (!spouse.gender.equals("male")) {
+            return name + " can only give birth if her spouse is a male";
+        }
+
+        children.add(child);
+        return name + " and " + spouse.name + " gave birth to a baby named " + child.name;
+    }
+
+    public String getJob(int money) {
+        if (employed != true) {
+            employed = true;
+            salary = money;
+            return name + " found a job that is paying " + (gender.equals("male") ? "him" : "her") + " "
+                    + salary + " per year";
+        } else {
+            return name + " already has a job";
+        }
+    }
+
+    public void leaveJob() {
+        if (employed == true) {
+            employed = false;
+            salary = 0;
+            System.out.println(name + " has left " + (gender.equals("male") ? "his" : "her") + " job");
+        } else {
+            System.out.println(
+                    name + " does not have a job " + (gender.equals("male") ? "he" : "she") + " can leave");
+        }
+    }
+
+    private int ageDiff(int otherAge) {
+        return otherAge >= this.age ? otherAge - this.age : this.age - otherAge;
+    }
+    
+    public String makeNewFriend(Human newFriend) {
+        double chanceOfBeingFriends = 0.75;
+        // for every year you differ in age you decrease your chances of being friends by 10%
+        int ageDiff = ageDiff(newFriend.age);
+        chanceOfBeingFriends -= AGE_PROBABILTY*ageDiff;
+        
+        // if you are a different ethnicity you decrease your changce of being friends by 30%
+        int sameNationality = this.ethnicity.equals(newFriend.ethnicity) ? 0 : 1;
+        chanceOfBeingFriends -= ETHNICITY_PROBABILITY * sameNationality;
+        
+        if (rand.nextDouble() <= chanceOfBeingFriends) {
+            
+        }
+        
+        return "Not yet supported";
+    }
+    
+    public String pursueMarried() {
+        return "Not yet supported";
+    }
+    
+    public String tryForKids(ArrayList<Human> newChildren) {
+        return "Not yet supported";
+    }
+    
+    public String checkVitals(ArrayList<Human> deceased) {
+        return "Not yet supported";
+    }
+    
 }
---
+++ Simulator.java
@@ -0,0 +1,58 @@
+package engl;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Simulator {
+    private static int startYear;
+    private static int currentYear;
+    
+    public static int getYear() {
+        return currentYear;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> simulationEvents = new ArrayList<String>();
+        ArrayList<Human> comunity = new ArrayList<Human>();
+        Human andrew, bethany, riley, abhijeet, ashwin, luc, kelsey, baby;
+
+        // Initialize the humans
+        comunity.add(new Human("Andrew", 23, "Vietnamese", "male", false, 0));
+        comunity.add(new Human("Bethany", 19, "Chinese", "female", true, 5000));
+        comunity.add(new Human("Riley", 23, "German", "male", false, 0));
+        comunity.add(new Human("Abhijeet", 23, "Indian", "male", false, 0));
+        comunity.add(new Human("Ashwin", 23, "Nepali", "male", true, 120000));
+        comunity.add(new Human("Luc", 27, "Vietnamese", "male", true, 70000));
+        comunity.add(new Human("Kelsey", 24, "Native American", "female", true, 50000));
+        comunity.add(new Human("Chuwali", 0, "Nepali", "female", false, 0));
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        int year = 2015;
+        while (Simulator.getYear() < 2115) {
+            ArrayList<Human> newChildren = new ArrayList<Human>();
+            ArrayList<Human> deceased = new ArrayList<Human>();
+            
+            // each year you meet a friend
+            Human newFriend;
+            for (Human person : comunity) {
+                // each year you spend time with someone in your community.
+                // if you aren't already friends you may become friends
+                newFriend = comunity.get(rand.nextInt(comunity.size()));
+                simulationEvents.add(person.makeNewFriend(newFriend));
+                
+                // this person may pursue marrage
+                simulationEvents.add(person.pursueMarried());
+                
+                // this person may try to have kids
+                simulationEvents.add(person.tryForKids(newChildren));
+                
+                // check to make sure the person survived another year    
+                simulationEvents.add(person.checkVitals(deceased));
+            }
+            comunity.remove(deceased);
+            comunity.add(newChildren);
+        }
+    }
+}
